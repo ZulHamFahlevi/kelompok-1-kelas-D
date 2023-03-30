5c523fd5e597d9f7cedc2af0a157558a96a5b989
Nickhayatun Firza Refiani <96812402+nfirzaaa@users.noreply.github.com>
2023-03-31 00:03:39 +0700

    Update File

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import HomePage from "./pages/homePage/homePage";
-import HybridClasses from "./pages/hybridClasses/hybridClasses";
-import KampusMerdeka from "./pages/kampusMerdeka/KampusMerdeka";
-import MyAccount from "./pages/myAccount/MyAccount";
+// import HomePage from "./pages/homePage/homePage";
+// import HybridClasses from "./pages/hybridClasses/hybridClasses";
+// import KampusMerdeka from "./pages/kampusMerdeka/KampusMerdeka";
+// import MyAccount from "./pages/myAccount/MyAccount";
 import RecordedLearning from "./pages/recordedLearning/RecordedLearning";
 
 function App() {
   return (
     <>
-      <HomePage />
-      <HybridClasses />
+      {/* <HomePage />
+      <HybridClasses /> */}
       <RecordedLearning />
-      <KampusMerdeka />
-      <MyAccount />
+      {/* <KampusMerdeka />
+      <MyAccount /> */}
     </>
   );
 }
